0ed49e85ba9a352b5fa197cebef044a3a451409a
Kristine Joy C. Cobrado <krstnjycobrado@gmail.com>
2025-05-19 04:44:24 +0800

    Update firebase.ts

--- config/firebase.ts
+++ config/firebase.ts
@@ -4,13 +4,13 @@ import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBZjkW_goxE3pnk9-GGTGiDnNYc_HxxL0g",
-  authDomain: "keeply-keepitneatly.firebaseapp.com",
-  projectId: "keeply-keepitneatly",
-  storageBucket: "keeply-keepitneatly.firebasestorage.app",
-  messagingSenderId: "1077290802024",
-  appId: "1:1077290802024:web:7095240648462434da14dd",
-  measurementId: "G-S6SB9VBQ9B"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 const app = initializeApp(firebaseConfig);
